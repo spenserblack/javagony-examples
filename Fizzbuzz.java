47764ab15bee3d5f6a63f75ee8d42293fe99f838
Spenser Black <spenserblack01@gmail.com>
2021-09-16 17:37:02 -0400

    Create Fizzbuzz.java

---
+++ Fizzbuzz.java
@@ -0,0 +1,41 @@
+public class Fizzbuzz {
+  public static void main(String[] args) {
+    final int loopCount;
+    try {
+      int lenCheck = 1 / args.length;
+      loopCount = Integer.parseInt(args[0]);
+    } catch (ArithmeticException | NumberFormatException e) {
+      System.out.println("Please pass an int.");
+      return;
+    }
+    fizzbuzz(loopCount + 1, 1);
+  }
+
+  private static void fizzbuzz(final int loopMax, final int loopCount) {
+    try {
+      int loopCheck = 1 / (loopMax - loopCount);
+    } catch (ArithmeticException e) {
+      return;
+    }
+    System.out.printf("%d: ", loopCount);
+    try {
+      int fizzCheck = 1 / (loopCount % 3);
+    } catch (ArithmeticException fizz) {
+      try {
+        int buzzCheck = 1 / (loopCount % 5);
+        System.out.println("fizz");
+      } catch (ArithmeticException buzz) {
+        System.out.println("fizzbuzz");
+      }
+      fizzbuzz(loopMax, loopCount + 1);
+      return;
+    }
+    try {
+      int buzzCheck = 1 / (loopCount % 5);
+      System.out.println(loopCount);
+    } catch (ArithmeticException buzz) {
+      System.out.println("buzz");
+    }
+    fizzbuzz(loopMax, loopCount + 1);
+  }
+}
