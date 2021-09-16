e04787d79142d2e647cd30c4fc503d4d8e77e9a2
Spenser Black <spenserblack01@gmail.com>
2021-09-16 18:00:08 -0400

    Add simple `for` alternative example

---
+++ ForLoop.java
@@ -0,0 +1,22 @@
+public class ForLoop {
+  public static void main(String []args) {
+    example(10);
+  }
+
+  /**
+   * Prints from 0 (inclusive) to max (exclusive).
+   */
+  public static void example(final int max) {
+    forLoop(0, max);
+  }
+
+  private static void forLoop(final int i, final int max) {
+    try {
+      final int loopCheck = 1 / (max - i);
+    } catch (ArithmeticException done) {
+      return;
+    }
+    System.out.println(i);
+    forLoop(i + 1, max);
+  }
+}
