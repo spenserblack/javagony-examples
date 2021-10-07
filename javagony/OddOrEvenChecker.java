f65eb215865b391c4c13fe98131649f4cc779664
Spenser Black <spenserblack01@gmail.com>
2021-10-07 11:56:00 -0400

    Fix scoping issue of `number`

--- javagony/OddOrEvenChecker.java
+++ javagony/OddOrEvenChecker.java
@@ -1,13 +1,17 @@
 public class OddOrEvenChecker {
     public static void main(String[] args) {
-        try{
-            int number = Integer.parseInt(args[0]);
-            int remainder = 1/(number%2);
-            System.out.printf("%d is odd\n", number);
+        final int number;
+        try {
+          number = Integer.parseInt(args[0]);
         } catch(ArrayIndexOutOfBoundsException | NumberFormatException e) {
             System.out.println("Please pass a number in the command-line");
             return;
-        } catch(ArithmeticException e){
+        }
+
+        try{
+            int remainder = 1/(number%2);
+            System.out.printf("%d is odd\n", number);
+        }  catch(ArithmeticException e){
             System.out.printf("%d is odd\n", number);
         }
     }
