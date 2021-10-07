5c1666247bc8e61acde4fdbdafd719ccaa18bb18
srideviabhirami <77732385+srideviabhirami@users.noreply.github.com>
2021-10-07 20:40:53 +0530

    Update OddOrEvenChecker.java
    Replace scanner with Integer Parser

--- javagony/OddOrEvenChecker.java
+++ javagony/OddOrEvenChecker.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class OddOrEvenChecker {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        int number = Integer.parseInt(args[0]);
         try{
             int remainder = 1/(number%2);
             System.out.print(number+" is odd");
